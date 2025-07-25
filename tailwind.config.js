62e036242ce2931ca61f3f3b7b2d861f9a6e8058
shamim anowar <shamim.hire@gmail.com>
2025-07-25 07:14:26 +0600

    Style: add some tailwind configs and refine later

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,7 +6,37 @@ module.exports = {
     "./app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      backgroundImage: {
+        "fame-gradient": "linear-gradient(90deg, #FC004E 0%, #10CBE0 100%)",
+      },
+      fontFamily: {
+        urbanist: ["Urbanist", "sans-serif"],
+        figtree: ["Figtree", "sans-serif"],
+      },
+      fontWeight: {
+        500: "500",
+        600: "600",
+        700: "700",
+        800: "800",
+        medium: "500",
+        semibold: "600",
+        bold: "700",
+        extrabold: "800",
+      },
+      colors: {
+        "fame-pink": "#FC004E",
+        "fame-cyan": "#00E7F9",
+      },
+    },
   },
   plugins: [],
+  safelist: [
+    "text-fame-cyan",
+    "text-fame-pink",
+    "bg-fame-gradient",
+    "font-figtree",
+    "font-urbanist",
+    "font-extrabold",
+  ],
 };
